8b07bbd92403f72c6f2457187b2bd1b0b93c9ade
Travis Hubbard <t3patterson@gmail.com>
2017-01-30 12:39:18 -0500

    solutions

--- object-iteration.js
+++ object-iteration.js
@@ -7,6 +7,24 @@
 //   and convert it into an array of strings with the name only.
 //------------------------------------------
 
+//INPUT: Array of Objects
+function partyBouncer(arrOfObjects){
+   var arrOfNames = []
+
+   for(var i = 0 ; i < arrOfObjects.length; i++){
+      var personObj = arrOfObjects[i]
+      if( personObj.age >= 18 && personObj.isFelon === false ){
+          arrOfNames.push( personObj.name )
+      }
+   }
+   
+   return arrOfNames
+   
+   //OUTPUT : Array of Strings
+}
+
+
+
 var listOfPeople = [
   { name: 'Jimmy Drayfus', age: 16, isFelon: true },
   { name: 'Sherry Tomkins', age: 33, isFelon: false },
@@ -26,10 +44,10 @@ var listOfPeople = [
 
 var guestList = partyBouncer(listOfPeople)
 
-console.assert(guestList.length === 6)
+console.assert(guestList.length === 7)
 console.assert(guestList[0] === "Sherry Tomkins")
 console.assert(guestList[1] === "Rufus Johnson")
-console.assert(guestList[5] === "Bobby Francois")
+console.assert(guestList[6] === "Bobby Francois")
 
 console.assert(guestList.indexOf('Jimmy Dreyfus') === -1)
 console.assert(guestList.indexOf('Romy Podolski') === -1)
@@ -37,7 +55,7 @@ console.assert(guestList.indexOf('Ahmad Funchess') === -1)
 
 
 
-
+console.log('+===========')
 
 // ---------------------------------------
 // EXERCISE 2 : keyMirror()
@@ -63,6 +81,18 @@ console.assert(guestList.indexOf('Ahmad Funchess') === -1)
 //   ...
 // }
 
+//INPUT : Object
+var keyMirror = function(inputObj){
+   var newObj = {}
+
+   for (var propString in inputObj){
+      var allCapsPropertyName = propString.toUpperCase()
+      newObj[ allCapsPropertyName ] = allCapsPropertyName
+   }
+   
+   return newObj
+   //OUTPUT : Transformed Object
+}
 
 var myActionNames = {
    GET_records: null,
